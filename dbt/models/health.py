01cdf83787567356e7dfb14b6ca186b07ff6a012
diogommartins <diogommartins.dev@gmail.com>
2021-09-28 10:49:21 -0300

    Adiciona amora data models

---
+++ dbt/models/health.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from sqlmodel import SQLModel, MetaData, Field
+
+
+class Health(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    type: str
+    sourceName: str
+    sourceVersion: str
+    unit: str
+    creationDate: datetime
+    startDate: datetime
+    endDate: datetime
+    value: float
+    device: str
+
+    metadata = MetaData(schema="amora-data-build-tool.diogo", quote_schema=True)
+
+
+def source() -> None:
+    return None
+
+
+output = Health
---
+++ dbt/models/heart_rate.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+from sqlalchemy import MetaData
+from sqlmodel import select, SQLModel, Field
+
+from amora.compilation import Compilable
+from amora.models import ModelConfig, PartitionConfig
+from dbt.models.health import Health
+
+
+class HeartRate(SQLModel, table=True):
+    __tablename__ = "heart_rate"
+    __depends_on__ = [Health]
+    __config__ = ModelConfig(
+        materialized="table",
+        partition_by=PartitionConfig(
+            field="creationDate", data_type="TIMESTAMP", granularity="day"
+        ),
+        cluster_by="sourceName",
+        tags=["daily"],
+    )
+
+    metadata = MetaData(schema="amora-data-build-tool.diogo")
+
+    creationDate: datetime
+    device: str
+    endDate: datetime
+    id: int = Field(primary_key=True)
+    sourceName: str
+    startDate: datetime
+    unit: str
+    value: float
+
+
+def source() -> Compilable:
+    return select(
+        [
+            Health.creationDate,
+            Health.device,
+            Health.endDate,
+            Health.id,
+            Health.sourceName,
+            Health.startDate,
+            Health.unit,
+            Health.value,
+        ]
+    ).where(Health.type == "HeartRate")
+
+
+output = HeartRate
