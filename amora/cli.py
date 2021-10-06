ee8c3bb6007ce66eead7e568f0ccb54840df5418
diogommartins <diogommartins.dev@gmail.com>
2021-10-06 12:38:13 -0300

    Recebe target como argumento de comandos da CLI

--- amora/cli.py
+++ amora/cli.py
@@ -9,23 +9,39 @@ app = typer.Typer(
 )
 
 Models = List[str]
+target_option = lambda: typer.Option(
+    ...,
+    "--target",
+    "-t",
+    help="Target connection configuration as defined as an amora.target.Target",
+)
 
 
 @app.command()
-def compile(models: Optional[Models] = typer.Argument(None)):
+def compile(
+    models: Optional[Models] = typer.Argument(None),
+    target: str = target_option(),
+) -> None:
     """
-    Generates executable SQL from model files. Compiled SQL files are written to the target/ directory.
+    Generates executable SQL from model files. Compiled SQL files are written to the `./target` directory.
     """
     typer.echo("compile")
 
 
 @app.command()
-def materialize(models: Optional[Models] = typer.Argument(None)):
+def materialize(
+    models: Optional[Models] = typer.Argument(None),
+    target: str = target_option(),
+) -> None:
     """
-    Executes the compiled SQL againts the curent target database.
+    Executes the compiled SQL againts the current target database.
     """
     typer.echo("materialize")
 
 
 def main():
     return app()
+
+
+if __name__ == "__main__":
+    main()
