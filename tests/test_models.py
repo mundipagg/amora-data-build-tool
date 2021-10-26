2efe3172a05c525cbd8e7bc1d8cf08ad65e70090
diogommartins <diogommartins.dev@gmail.com>
2021-10-26 11:37:03 -0300

    DTREX-246 unittest -> pytest

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,31 +1,27 @@
-import unittest
-from pathlib import Path
+import inspect
 
-from amora.compilation import Compilable
-from amora.config import settings
+from amora.compilation import compile_statement
 from tests.models.health import Health
 from tests.models.steps import Steps
 
 
-class ModelTests(unittest.TestCase):
-    def setUp(self) -> None:
-        tests_path = Path(__file__).parent
+def test_target_path():
+    path = Health.target_path(model_file_path=inspect.getfile(Health))
+    assert path.as_posix().endswith("target/health.sql")
 
-        settings.dbt_models_path = tests_path.joinpath("models").as_posix()
-        settings.target_path = tests_path.joinpath("target").as_posix()
 
-    def test_target_path(self):
-        path = Health.target_path()
-        self.assertTrue(path.as_posix().endswith("target/health.sql"))
+def test_model_without_dependencies():
+    assert Health.dependencies() == []
 
-    def test_model_without_dependencies(self):
-        self.assertEqual(Health.dependencies(), [])
 
-    def test_model_with_dependencies(self):
-        self.assertEqual(Steps.dependencies(), [Health])
+def test_model_with_dependencies():
+    assert Steps.dependencies() == [Health]
 
-    def test_model_without_source(self):
-        self.assertIsNone(Health.source())
 
-    def test_model_with_compilable_source(self):
-        self.assertIsInstance(Steps.source(), Compilable)
+def test_model_without_source():
+    assert Health.source() is None
+
+
+def test_model_with_compilable_source():
+    assert Steps.source() is not None
+    assert isinstance(compile_statement(Steps.source()), str)
