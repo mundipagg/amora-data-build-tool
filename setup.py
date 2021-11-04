c951b4f2f5de9f814bcf102b1b1e3ed1defa1c70
Diogo Magalhães Martins <diogo.martins@stone.com.br>
2021-11-04 13:54:37 -0300

    Corrige especificação de networks na lista de dependências do setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "jupyter~=1.0.0",
         "matplotlib~=3.4.2",
-        "networkx[all]~=2.6.3",
+        "networkx[default,extra]~=2.6.3",
         "numpy~=1.21.1",
         "pandas~=1.3.0",
         "sqlalchemy-bigquery~=1.2.0",
