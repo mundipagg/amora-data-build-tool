80c31a5c357c94bfda3f29e6d66612ed277f54b1
diogommartins <diogommartins.dev@gmail.com>
2021-11-12 13:49:26 -0300

    DTREX-310 Adiciona implementações para asserções de testes

--- amora/tests/assertions.py
+++ amora/tests/assertions.py
@@ -2,28 +2,20 @@ from typing import Iterable
 
 import pytest
 from mypy.api import Callable
-from sqlalchemy import func
+from sqlalchemy import func, and_, Table
 from sqlalchemy.orm import InstrumentedAttribute
 from sqlmodel.sql.expression import SelectOfScalar
 
-from amora.models import select, Compilable
+from amora.models import select, Compilable, AmoraModel
 from amora.compilation import compile_statement
 from amora.providers.bigquery import get_client
 
 Column = InstrumentedAttribute
+Test = Callable[[Column, ...], SelectOfScalar]
 
 
-def that(
-    column: Column,
-    test: Callable[[Column, ...], SelectOfScalar],
-    **test_kwargs,
-):
-    """
-    >>> assert that(HeartRate.value, is_not_null)
-
-    Executes the test, returning True if the test is successful and raising a pytest fail otherwise
-    """
-    sql_stmt = compile_statement(statement=test(column, **test_kwargs))
+def _test(statement: Compilable) -> bool:
+    sql_stmt = compile_statement(statement=statement)
 
     query_job = get_client().query(sql_stmt)
     result = query_job.result()
@@ -31,7 +23,27 @@ def that(
     if result.total_rows == 0:
         return True
     else:
-        pytest.fail(f"{result.total_rows} rows failed the test assertion.")
+        pytest.fail(
+            f"{result.total_rows} rows failed the test assertion."
+            f"\n==========="
+            f"\nTest query:"
+            f"\n==========="
+            f"\n{sql_stmt}",
+            pytrace=False,
+        )
+
+
+def that(
+    column: Column,
+    test: Test,
+    **test_kwargs,
+) -> bool:
+    """
+    >>> assert that(HeartRate.value, is_not_null)
+
+    Executes the test, returning True if the test is successful and raising a pytest fail otherwise
+    """
+    return _test(statement=test(column, **test_kwargs))
 
 
 def is_not_null(column: Column) -> Compilable:
@@ -48,7 +60,7 @@ def is_not_null(column: Column) -> Compilable:
         WHERE {{ column_name }} IS NULL
     ```
     """
-    return select(column.parent.entity).where(column == None)
+    return select(column).where(column == None)
 
 
 def is_unique(column: Column) -> Compilable:
@@ -68,7 +80,7 @@ def is_unique(column: Column) -> Compilable:
         ) validation_errors
     ```
     """
-    return select(column.parent.entity).group_by(column).having(func.count(column) > 1)
+    return select(column).group_by(column).having(func.count(column) > 1)
 
 
 def has_accepted_values(column: Column, values: Iterable) -> Compilable:
@@ -78,22 +90,54 @@ def has_accepted_values(column: Column, values: Iterable) -> Compilable:
     The `source` column in the `HeartRate` model should be one of
     'iPhone' or 'MiBand'
     """
-    return select(column.parent.entity).where(~column.in_(values))
+    return select(column).where(~column.in_(values))
 
 
-def relates_to(column: Column, to: Column) -> Compilable:
+def relationship(
+    from_: Column, to: Column, from_condition=and_(True), to_condition=and_(True)
+) -> Compilable:
     """
-    >>> relates_to(HeartRate.id, to=Health.id)
+    >>> relationship(HeartRate.id, to=Health.id)
 
     Each `id` in the `HeartRate` model exists as an `id` in the `Health`
     table (also known as referential integrity)
+
+    This test validates the referential integrity between two relations
+    (same as the core relationships schema test) with an added predicate
+    to filter out some rows from the test. This is useful to exclude
+    records such as test entities, rows created in the last X minutes/hours
+    to account for temporary gaps due to data ingestion limitations, etc.
+
     """
-    pass
+    left_table = (
+        select(from_.label("id"))
+        .where(from_ != None)
+        .where(from_condition)
+        .cte("left_table")
+    )
+    right_table = (
+        select(to.label("id")).where(to != None).where(to_condition).cte("right_table")
+    )
+
+    exceptions = (
+        select([left_table.c["id"], right_table.c["id"]])
+        .select_from(
+            left_table.join(
+                right_table,
+                onclause=left_table.c["id"] == right_table.c["id"],
+                isouter=True,
+            )
+        )
+        .where(right_table.c["id"] == None)
+    )
+
+    return _test(statement=exceptions)
 
 
 def is_non_negative(column: Column) -> Compilable:
     """
     >>> is_non_negative(HeartRate.value)
+    True
 
     Each not null `value` in `HeartRate` model is >= 0
 
@@ -104,3 +148,16 @@ def is_non_negative(column: Column) -> Compilable:
     ```
     """
     return select(column.parent.entity).where(column < 0)
+
+
+def expression_is_true(expression, condition=and_(True)) -> bool:
+    """
+    >>> expression_is_true(StepsAgg._sum > StepsAgg._avg, condition=StepsAgg.year == 2021)
+
+    Asserts that a expression is True for all records.
+    This is useful when checking integrity across columns, for example,
+    that a total is equal to the sum of its parts, or that at least one column is true.
+
+    ```
+    """
+    return _test(statement=select(["*"]).where(condition).where(~expression))
