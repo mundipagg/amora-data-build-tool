632dbfa2a95aad62e3975c5c988c8db32971188e
diogommartins <diogommartins.dev@gmail.com>
2021-11-12 13:50:07 -0300

    DTREX-310 Adiciona testes para os modelos de exemplo steps e health

--- examples/amora_project/tests/test_health.py
+++ examples/amora_project/tests/test_health.py
@@ -1,5 +1,12 @@
-from amora.tests.assertions import is_not_null, that, is_non_negative, has_accepted_values
-from example_project.models.health import Health
+from amora.tests.assertions import (
+    is_not_null,
+    that,
+    is_non_negative,
+    has_accepted_values,
+    expression_is_true,
+    is_unique,
+)
+from examples.amora_project.models.health import Health
 
 
 def test_value_is_not_null():
@@ -15,12 +22,32 @@ def test_type_is_not_null():
 
 
 def test_type_has_accepted_values():
-    assert that(Health.type, has_accepted_values, values=[
-        "HeartRate",
-        "StepCount",
-        "DistanceWalkingRunning",
-        "FlightsClimbed",
-        "HeadphoneAudioExposure",
-        "WalkingDoubleSupportPercentage",
-        "WalkingSpeed",
-    ])
+    assert that(
+        Health.type,
+        has_accepted_values,
+        values=[
+            "ActiveEnergyBurned",
+            "BodyMass",
+            "BodyMassIndex",
+            "DistanceWalkingRunning",
+            "FlightsClimbed",
+            "HKDataTypeSleepDurationGoal",
+            "HeadphoneAudioExposure",
+            "HeartRate",
+            "Height",
+            "SleepAnalysis",
+            "StepCount",
+            "WalkingAsymmetryPercentage",
+            "WalkingDoubleSupportPercentage",
+            "WalkingSpeed",
+            "WalkingStepLength",
+        ],
+    )
+
+
+def test_end_date_after_start_date():
+    assert expression_is_true(Health.endDate >= Health.startDate)
+
+
+def test_id_is_unique():
+    assert that(Health.id, is_unique)
