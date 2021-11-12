156c57d0b6b4da28bd609de23ac8b714f999ff7b
diogommartins <diogommartins.dev@gmail.com>
2021-11-12 15:24:22 -0300

    DTREX-310 Adiciona testes ao projeto de exemplo

---
+++ examples/amora_project/tests/test_heart_rate.py
@@ -0,0 +1,25 @@
+from amora.tests.assertions import (
+    relationship,
+    that,
+    is_non_negative,
+    has_accepted_values,
+    expression_is_true,
+)
+from examples.amora_project.models.health import Health
+from examples.amora_project.models.heart_rate import HeartRate
+
+
+def test_relates_to_health():
+    assert relationship(from_=HeartRate.id, to=Health.id)
+
+
+def test_value_is_non_negative():
+    assert that(HeartRate.value, is_non_negative)
+
+
+def test_unit_accepted_values():
+    assert that(HeartRate.unit, has_accepted_values, values=["count/min"])
+
+
+def test_end_date_after_start_date():
+    assert expression_is_true(HeartRate.endDate >= HeartRate.startDate)
