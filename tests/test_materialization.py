42e04cad0017111e22fdd0e03424f886008469f6
diogommartins <diogommartins.dev@gmail.com>
2021-12-09 15:03:30 -0300

    Adiciona suporte a materialização de views e tabelas com descrição, labels e clusterização

--- tests/test_materialization.py
+++ tests/test_materialization.py
@@ -9,8 +9,10 @@ from amora.models import (
     ModelConfig,
     PartitionConfig,
     MaterializationTypes,
+    Field,
 )
 from amora.config import settings
+from amora.providers.bigquery import Client, QueryJobConfig, Table
 
 from tests.models.heart_agg import HeartRateAgg
 from tests.models.heart_rate import HeartRate
@@ -46,23 +48,28 @@ def test_dependency_dags_is_iterable_and_topologicaly_sorted():
     dag = DependencyDAG.from_tasks(tasks)
     topologicaly_sorted_tasks = list(dag)
 
-    assert topologicaly_sorted_tasks[0] == "Health"
-    assert topologicaly_sorted_tasks[-1] == "HeartRateAgg"
+    assert (
+        topologicaly_sorted_tasks[0] == "amora-test-project.testschema.health"
+    )
+    assert (
+        topologicaly_sorted_tasks[-1]
+        == "amora-test-project.testschema.heart_rate_agg"
+    )
 
 
-@patch("amora.materialization.Client")
+@patch("amora.materialization.Client", spec=Client)
 def test_materialize_as_view(Client: MagicMock):
-    class ViewModel(AmoraModel):
-        x: int
-        y: int
+    class ViewModel(AmoraModel, table=True):
+        x: int = Field(primary_key=True)
+        y: int = Field(primary_key=True)
 
         __model_config__ = ModelConfig(
             materialized=MaterializationTypes.view,
             partition_by=PartitionConfig(
                 field="creationDate", data_type="TIMESTAMP", granularity="day"
             ),
-            cluster_by="sourceName",
-            tags=["daily"],
+            cluster_by=["sourceName"],
+            labels={"freshness": "daily"},
         )
 
     result = materialize(sql="SELECT 1", model=ViewModel)
@@ -71,16 +78,18 @@ def test_materialize_as_view(Client: MagicMock):
     assert client.create_table.call_count == 1
     assert client.create_table.return_value == result
 
+    view: Table = client.create_table.call_args_list[0][0][0]
+    assert view.description == ViewModel.__model_config__.description
+    assert view.clustering_fields == ViewModel.__model_config__.cluster_by
+    assert view.labels == ViewModel.__model_config__.labels
 
-@patch("amora.materialization.Client")
-@patch("amora.materialization.QueryJobConfig")
+
+@patch("amora.materialization.Client", spec=Client)
+@patch("amora.materialization.QueryJobConfig", spec=QueryJobConfig)
 def test_materialize_as_table(QueryJobConfig: MagicMock, Client: MagicMock):
     table_name = uuid4().hex
-    table_id = (
-        f"{settings.TARGET_PROJECT}.{settings.TARGET_SCHEMA}.{table_name}"
-    )
 
-    class TableModel(AmoraModel):
+    class TableModel(AmoraModel, table=True):
         __tablename__ = table_name
         __model_config__ = ModelConfig(
             materialized=MaterializationTypes.table,
@@ -88,43 +97,55 @@ def test_materialize_as_table(QueryJobConfig: MagicMock, Client: MagicMock):
                 field="created_at", data_type="TIMESTAMP", granularity="day"
             ),
             cluster_by=["x", "y"],
-            tags=["daily"],
+            labels={"freshness": "daily"},
+            description=uuid4().hex,
         )
 
-        x: int
-        y: int
-        created_at: datetime
+        x: int = Field(primary_key=True)
+        y: int = Field(primary_key=True)
+        created_at: datetime = Field(primary_key=True)
 
     result = materialize(sql="SELECT 1", model=TableModel)
 
     client = Client.return_value
 
-    assert client.get_table.return_value == result
-    client.get_table.assert_called_once_with(table_id)
+    client.get_table.assert_called_once_with(TableModel.unique_name)
+
     client.query.assert_called_once_with(
         "SELECT 1",
         job_config=QueryJobConfig(
-            destination=table_id, write_disposition="WRITE_TRUNCATE"
+            destination=TableModel.unique_name,
+            write_disposition="WRITE_TRUNCATE",
         ),
     )
 
+    table: Table = client.get_table.return_value
+    client.update_table.assert_called_once_with(
+        table,
+        ["description", "labels", "clustering_fields"],
+    )
 
-@patch("amora.materialization.Client")
+    assert table.description == TableModel.__model_config__.description
+    assert table.clustering_fields == TableModel.__model_config__.cluster_by
+    assert table.labels == TableModel.__model_config__.labels
+
+
+@patch("amora.materialization.Client", spec=Client)
 def test_materialize_as_ephemeral(Client: MagicMock):
     table_name = uuid4().hex
     table_id = (
         f"{settings.TARGET_PROJECT}.{settings.TARGET_SCHEMA}.{table_name}"
     )
 
-    class EphemeralModel(AmoraModel):
+    class EphemeralModel(AmoraModel, table=True):
         __tablename__ = table_name
         __model_config__ = ModelConfig(
             materialized=MaterializationTypes.ephemeral,
         )
 
-        x: int
-        y: int
-        created_at: datetime
+        x: int = Field(primary_key=True)
+        y: int = Field(primary_key=True)
+        created_at: datetime = Field(primary_key=True)
 
     assert materialize(sql="SELECT 1", model=EphemeralModel) is None
     assert not Client.called
