9b18a4cf90415493df804fa41f695330ce527d48
diogommartins <diogommartins.dev@gmail.com>
2021-12-09 15:04:38 -0300

    Refatora exemplo amora_project, trocando uso de tags por labels

--- tests/models/heart_rate.py
+++ tests/models/heart_rate.py
@@ -20,8 +20,8 @@ class HeartRate(AmoraModel, table=True):
         partition_by=PartitionConfig(
             field="creationDate", data_type="TIMESTAMP", granularity="day"
         ),
-        cluster_by="sourceName",
-        tags=["daily"],
+        cluster_by=["sourceName"],
+        labels={"freshness": "daily"},
     )
 
     creationDate: datetime
