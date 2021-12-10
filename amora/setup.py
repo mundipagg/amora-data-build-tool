5e67080d11d13b52d09227110afefe3223d13193
diogommartins <diogommartins.dev@gmail.com>
2021-12-10 16:24:09 -0300

    0.1.5rc1

--- amora/setup.py
+++ amora/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="amora",
-    version="0.1.5",
+    version="0.1.5rc1",
     description="Amora Data Build Tool",
     url="http://github.com/mundipagg/amora-data-build-tool",
     author="TREX Data",
