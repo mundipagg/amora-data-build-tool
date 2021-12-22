a9fcc0ebc3f1775fe40b7734ec6e860a86317350
diogommartins <diogommartins.dev@gmail.com>
2021-12-22 17:06:44 -0300

    Melhora documentação

--- examples/amora_project/models/health.py
+++ examples/amora_project/models/health.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
-from amora.models import AmoraModel
+from amora.models import AmoraModel, ModelConfig, MaterializationTypes
 from sqlmodel import Field
 
 
 class Health(AmoraModel, table=True):
+    __model_config__ = ModelConfig(
+        materialized=MaterializationTypes.table,
+        description="Health data exported by the Apple Health App",
+    )
+
     id: int = Field(primary_key=True)
     type: str
     sourceName: str
