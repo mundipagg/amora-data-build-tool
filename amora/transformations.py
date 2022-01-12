8db0f12dc501869d15865902a85e5f0a148891c8
diogommartins <diogommartins.dev@gmail.com>
2022-01-12 14:49:20 -0300

    Adiciona amora.transaformations.parse_numbers

--- amora/transformations.py
+++ amora/transformations.py
@@ -18,3 +18,15 @@ def remove_leading_zeros(column: Column) -> Function:
      E.g: "00001000000" -> "1000000"
     """
     return func.regexp_replace(column, "^0+", "", type_=String)
+
+
+def parse_numbers(column: Column) -> Function:
+    """
+    Parses a string column as a number, returning NULL if value contains 0 numbers
+
+    E.g: "0031.752.270/0001-82" -> "31752270000182"
+         "IM_A_STRING_WITH_NO_NUMBERS" -> NULL
+    """
+    return func.nullif(
+        remove_leading_zeros(remove_non_numbers(column)), "", type_=String
+    )
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,13 +1,18 @@
 from amora.models import select
 from amora.providers.bigquery import cte_from_rows, run
 from amora.tests.assertions import is_numeric, that
-from amora.transformations import remove_non_numbers, remove_leading_zeros
+from amora.transformations import (
+    remove_non_numbers,
+    remove_leading_zeros,
+    parse_numbers,
+)
 
 
 def test_remove_non_numbers():
     cte = cte_from_rows(
         [
             {"col": "a123"},
+            {"col": "aaaa"},
             {"col": "1a23"},
             {"col": "123a"},
             {"col": "123.456.789-45"},
@@ -25,6 +30,7 @@ def test_remove_non_numbers():
 
     assert [tuple(row) for row in run(statement).rows] == [
         ("a123", "123"),
+        ("aaaa", ""),
         ("1a23", "123"),
         ("123a", "123"),
         ("123.456.789-45", "12345678945"),
@@ -67,4 +73,33 @@ def test_remove_leading_zeros():
             "00009223372036854775807",
             "9223372036854775807",
         ),
+    ]
+
+
+def test_parse_numbers():
+    cte = cte_from_rows(
+        [
+            {"col": "aaa"},
+            {"col": "1a23"},
+            {"col": "123a"},
+            {"col": "123.456.789-45"},
+            {"col": "+55(21)123456-78945"},
+            {"col": "00031.752.270/0001-82"},
+        ]
+    )
+
+    statement = select(
+        cte.c.col,
+        parse_numbers(cte.c.col).label("col_with_numbers_only"),
+    )
+
+    assert that(statement.cte().c.col_with_numbers_only, is_numeric)
+
+    assert [tuple(row) for row in run(statement).rows] == [
+        ("aaa", None),
+        ("1a23", "123"),
+        ("123a", "123"),
+        ("123.456.789-45", "12345678945"),
+        ("+55(21)123456-78945", "552112345678945"),
+        ("00031.752.270/0001-82", "31752270000182"),
     ]
