d00fb8b0df19d71c59bcca7dfdfff63439b00df1
diogommartins <diogommartins.dev@gmail.com>
2022-01-28 15:24:55 -0300

    Adiciona validação `is_a_non_empty_string`

--- tests/test_assertions.py
+++ tests/test_assertions.py
@@ -1,8 +1,10 @@
-import pytest
-from google.api_core.exceptions import BadRequest
-
 from amora.providers.bigquery import cte_from_rows
-from amora.tests.assertions import is_numeric, that
+from amora.tests.assertions import (
+    is_numeric,
+    that,
+    has_at_least_one_not_null_value,
+    is_a_non_empty_string,
+)
 
 
 def test_is_numeric_with_numeric_column():
@@ -38,3 +40,57 @@ def test_is_numeric_with_non_numeric_values_on_column():
     )
 
     assert not that(cte.c.numeric_column, is_numeric, raise_on_fail=False)
+
+
+def test_has_at_least_one_not_null_value_with_non_null_value():
+    cte = cte_from_rows(
+        [
+            {"col": None},
+            {"col": None},
+            {"col": "356"},
+            {"col": None},
+        ]
+    )
+
+    assert that(cte.c.col, has_at_least_one_not_null_value)
+
+
+def test_has_at_least_one_not_null_value_without_non_null_value():
+    cte = cte_from_rows(
+        [
+            {"col": None},
+            {"col": None},
+            {"col": None},
+            {"col": None},
+        ]
+    )
+
+    assert not that(
+        cte.c.col, has_at_least_one_not_null_value, raise_on_fail=False
+    )
+
+
+def test_is_a_non_empty_string():
+    cte = cte_from_rows(
+        [
+            {"col": "abc"},
+            {"col": "asdasdas  "},
+            {"col": "a"},
+            {"col": "b"},
+        ]
+    )
+
+    assert that(cte.c.col, is_a_non_empty_string)
+
+
+def test_is_a_non_empty_string_with_empty_strings():
+    cte = cte_from_rows(
+        [
+            {"col": "abc"},
+            {"col": "asdasdas  "},
+            {"col": ""},
+            {"col": "      "},
+        ]
+    )
+
+    assert not that(cte.c.col, is_a_non_empty_string, raise_on_fail=False)
