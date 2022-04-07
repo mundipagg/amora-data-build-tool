6bc5556286ded48461e21a0c04ff2e559a451175
diogommartins <diogommartins.dev@gmail.com>
2022-04-07 11:07:46 -0300

    DTREX-406 Remove opção `--model` da CLI `amora feature-store materialize`

--- amora/cli.py
+++ amora/cli.py
@@ -409,7 +409,7 @@ def feature_store_materialize(
     ),
     end_ts: str = typer.Argument(
         None, help="End timestamp on ISO 8601 format. E.g.: '2022-01-02T01:00:00'"
-    )
+    ),
 ):
     """
     Run a (non-incremental) materialization job to ingest data into the online
