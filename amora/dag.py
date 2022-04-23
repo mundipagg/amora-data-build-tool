41e5d3513016fcd0e0730e33970f9beb372144d6
diogommartins <diogommartins.dev@gmail.com>
2022-04-23 20:24:01 -0300

    DTREX-608 Adiciona visualização para `DependencyDAG` de um projeto em `amora.dash.dependency_dag`

--- amora/dag.py
+++ amora/dag.py
@@ -43,6 +43,31 @@ class DependencyDAG(nx.DiGraph):
 
         return cls.from_tasks(tasks=model_to_task.values())
 
+    def to_cytoscape_elements(self) -> List[Dict]:
+        """
+
+        Returns itself as a cytoscape schema compatible representation. E.g:
+
+        For a `A --> B` graph:
+
+        ```python
+        [
+            {"data": {"id": "A", "label": "A"}},
+            {"data": {"id": "B", "label": "B"}},
+            {"data": {"source": "A", "target": "B"}}
+        ]
+        ```
+        """
+        # todo: Estilizar nó de acordo com o tipo de materialização
+        # todo: Adicionar metadados para cytoscape
+        return [
+            *({"data": {"id": node, "label": node}} for node in self.nodes),
+            *(
+                {"data": {"source": source, "target": target}}
+                for source, target in self.edges
+            ),
+        ]
+
     def draw(self) -> None:
         plt.figure(1, figsize=settings.CLI_MATERIALIZATION_DAG_FIGURE_SIZE)
         nx.draw(
