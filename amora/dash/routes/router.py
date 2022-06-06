1e0fa596088768c782be180702c6848e53d7bd5b
diogommartins <diogommartins.dev@gmail.com>
2022-06-06 11:04:39 -0300

    Adds `amora.dash.router`

---
+++ amora/dash/routes/router.py
@@ -0,0 +1,24 @@
+from typing import Callable, Dict
+
+from dash import html
+from dash.development.base_component import Component
+
+from amora.dash.routes import (
+    environement,
+    feature_store,
+    home,
+    models,
+    questions,
+    tests,
+)
+
+ROUTER: Dict[str, Callable[..., Component]] = {
+    "/": home.content,
+    "/search": lambda: html.P("This is the content of page 1. Yay!"),
+    "/models": models.content,
+    "/tests": tests.content,
+    "/questions": questions.content,
+    "/whiteboard": lambda: html.P("Whiteboard"),
+    "/environment": environement.content,
+    "/feature-store": feature_store.content,
+}
