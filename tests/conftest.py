5917fd079378b1546d6f754da65e38d2b6e684d7
diogommartins <diogommartins.dev@gmail.com>
2022-06-08 14:48:23 -0300

    Adds selenium to dev dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
 from pathlib import Path
 
+from selenium.webdriver.chrome.options import Options
+
 from amora.config import settings
 from amora.utils import clean_compiled_files
+
+
+def pytest_setup_options():
+    options = Options()
+    options.add_argument("--disable-gpu")
+    return options
 
 
 def pytest_sessionstart(session):
