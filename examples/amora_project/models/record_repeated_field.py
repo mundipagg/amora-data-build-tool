ea08ee726a599eb895e12326706ffc3842f16c1a
diogommartins <diogommartins.dev@gmail.com>
2022-06-21 00:48:33 -0300

    feat(examples): Adds an example model that uses bigquery's repeated fields struct

---
+++ examples/amora_project/models/record_repeated_field.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from amora.models import AmoraModel, Field, MaterializationTypes, ModelConfig, select
+from amora.providers.bigquery import array, cte_from_rows
+from amora.types import Compilable
+
+
+class Node(AmoraModel):
+    id: str
+
+
+class Edge(AmoraModel):
+    from_node: Node
+    to_node: Node
+
+
+class RecordRepeatedFields(AmoraModel, table=True):
+    __model_config__ = ModelConfig(materialized=MaterializationTypes.view)
+
+    id: int = Field(primary_key=True)
+    nodes: List[Node]
+    edges: List[Edge]
+    root_node: Node
+
+    @classmethod
+    def source(cls) -> Compilable:
+        rows = cte_from_rows(
+            [
+                {
+                    "id": 1,
+                    "nodes": array([Node(id="a"), Node(id="b"), Node(id="c")]),
+                    "edges": array(
+                        [
+                            Edge(from_node="a", to_node="b"),
+                            Edge(from_node="b", to_node="c"),
+                        ]
+                    ),
+                    "root_node": Node(id="a")
+                },
+            ]
+        )
+        return select(rows)
