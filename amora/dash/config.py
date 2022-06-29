debda55efa01e00a0dd1a166cc47946b2a1ac237
diogommartins <diogommartins.dev@gmail.com>
2022-06-29 16:16:09 -0300

    feat(dash): Adds `amora.dash.authentication` with Auth0 integration for authentication

--- amora/dash/config.py
+++ amora/dash/config.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Optional
+from uuid import uuid4
 
 import dash_bootstrap_components as dbc
 from pydantic import BaseSettings, validator
@@ -10,8 +11,27 @@ class DashSettings(BaseSettings):
     DBC_THEME: str = "MATERIA"
     DEBUG: bool = False
 
+    APP_SECRET_KEY: str = uuid4().hex
+
+    AUTH0_CLIENT_ID: Optional[str] = None
+    AUTH0_CLIENT_SECRET: Optional[str] = None
+    AUTH0_DOMAIN: Optional[str] = None
+    AUTH0_LOGIN_PATH: str = "/login"
+    AUTH0_LOGOUT_PATH: str = "/logout"
+    AUTH0_CALLBACK_PATH: str = "/callback"
+    AUTH0_CALLBACK_REDIRECT_PATH: str = "/"
+    AUTH0_SCOPE: str = "openid profile email"
+
     class Config:
         env_prefix = "AMORA_DASH_"
+
+    @property
+    def auth0_login_enabled(self) -> bool:
+        return (
+            bool(self.AUTH0_CLIENT_ID)
+            and bool(self.AUTH0_CLIENT_SECRET)
+            and bool(self.AUTH0_DOMAIN)
+        )
 
     @property
     def dbc_theme_stylesheet(self) -> str:
