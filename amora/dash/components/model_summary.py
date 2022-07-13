111518d93804d597e9230debf9e2d70addaca9f3
diogommartins <diogommartins.dev@gmail.com>
2022-07-12 21:06:29 -0300

    fix(dash): Fixes `model_summary.component` API and visualization
    - Expects an `AmoraModel`, instead of a `pd.DataFrame`
    - Renders `column_name` as the first column of the table

--- amora/dash/components/model_summary.py
+++ amora/dash/components/model_summary.py
@@ -1,17 +1,27 @@
-import pandas as pd
 from dash import dash_table
 from dash.development.base_component import Component
 
+from amora.meta_queries import summarize
+from amora.models import Model
 
-def component(summary: pd.DataFrame) -> Component:
+
+def component(model: Model) -> Component:
+    summary = summarize(model)
+
+    first_column = "column_name"
     return dash_table.DataTable(
         id="model-summary",
-        columns=[
+        columns=[{"name": first_column, "id": first_column, "selectable": True}]
+        + [
             {"name": col, "id": col, "selectable": True}
             for col in sorted(summary.columns.values)
+            if col != first_column
         ],
         data=summary.to_dict("records"),
         row_selectable="multi",
+        sort_mode="multi",
+        sort_by=[{"column_id": first_column, "direction": "asc"}],
+        sort_action="native",
         hidden_columns=["is_fv_feature", "is_fv_entity", "is_fv_event_timestamp"],
         style_cell={"overflow": "hidden", "textOverflow": "ellipsis", "maxWidth": 0},
         style_data_conditional=[
