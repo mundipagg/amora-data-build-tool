ddf06e5539d76a1b88de1f5796830f29a8be9185
diogommartins <diogommartins.dev@gmail.com>
2022-07-13 14:57:56 -0300

    feat(dash): Adds `amora.dash.components.model_columns`

---
+++ amora/dash/components/model_columns.py
@@ -0,0 +1,28 @@
+import dash_table
+import pandas as pd
+from dash import html
+from dash.development.base_component import Component
+
+from amora.models import Model
+
+
+def component(model: Model) -> Component:
+    df = pd.DataFrame(
+        [
+            {
+                "column": col.key,
+                "description": col.comment if col.comment else "Undocumented",
+            }
+            for col in model.__table__.columns
+        ]
+    )
+    return html.Div(
+        dash_table.DataTable(
+            data=df.to_dict("records"),
+            columns=[
+                {"name": "column", "id": "column"},
+                {"name": "description", "id": "description"},
+            ],
+        ),
+        id=f"model-columns-{model.unique_name}",
+    )
