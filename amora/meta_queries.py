25a7f6cc13111d141bac1a4e4014619680749bef
Thúlio Costa <thulio.patriota@stone.com.br>
2022-07-30 12:25:37 -0300

    feat: Otimiza sumarização de modelos

--- amora/meta_queries.py
+++ amora/meta_queries.py
@@ -2,75 +2,84 @@ from datetime import date
 
 import pandas as pd
 from numpy import nan
-from sqlalchemy import ARRAY, Float, Integer, Numeric, String, cast, func, literal
+from sqlalchemy import (
+    ARRAY,
+    Float,
+    Integer,
+    Numeric,
+    String,
+    cast,
+    func,
+    literal,
+    union_all,
+)
+from sqlalchemy_bigquery import STRUCT
 
 from amora.feature_store.protocols import FeatureViewSourceProtocol
-from amora.models import Column, Model, select
+from amora.models import Model, select
 from amora.providers.bigquery import run
 from amora.storage import cache
 
 
 @cache(suffix=lambda model: f"{model.unique_name}.{date.today()}")
 def summarize(model: Model) -> pd.DataFrame:
-    return pd.concat(
-        [summarize_column(model, column) for column in model.__table__.columns]
-    )
+    return _summarize_columns(model)
 
 
-@cache(suffix=lambda model, column: f"{model.unique_name}.{column.name}.{date.today()}")
-def summarize_column(model: Model, column: Column) -> pd.DataFrame:
-    df = pd.DataFrame(
-        columns=[
-            "column_name",
-            "column_type",
-            "min",
-            "max",
-            "avg",
-            "unique_count",
-            "null_percentage",
-            "stddev",
-        ]
-    )
+def _summarize_columns(model: Model) -> pd.DataFrame:
+    stmts = []
 
-    is_array = isinstance(column.type, ARRAY)
-    if is_array:
-        # fixme:
-        return pd.DataFrame()
+    for column in model.__table__.columns:
+        is_supported = not isinstance(column.type, (ARRAY, STRUCT))
+        is_numeric = isinstance(column.type, (Numeric, Integer, Float))
 
-    is_numeric = isinstance(column.type, (Numeric, Integer, Float))
+        if isinstance(model, FeatureViewSourceProtocol):
+            is_fv_feature = column.name in (
+                c.name for c in model.feature_view_features()
+            )
+            is_fv_entity = column.name in (
+                c.name for c in model.feature_view_entities()
+            )
+            is_fv_event_timestamp = (
+                column.name == model.feature_view_event_timestamp().name
+            )
 
-    stmt = select(
-        cast(func.min(column), String).label("min"),
-        cast(func.max(column), String).label("max"),
-        func.count(column.distinct()).label("unique_count"),
-        (cast(func.avg(column), String) if is_numeric else literal(None)).label("avg"),  # type: ignore
-        (func.stddev(column) if is_numeric else literal(None)).label("stddev"),  # type: ignore
-        func.safe_divide(
-            (literal(100) * func.countif(column == None)), func.count(column)
-        ).label("null_percentage"),
-    )
-    result = run(stmt)
+        else:
+            is_fv_feature = False
+            is_fv_entity = False
+            is_fv_event_timestamp = False
 
-    df = df.append(result.to_dataframe(), ignore_index=True)
-    df["column_name"] = column.name
-    df["column_type"] = str(column.type)
-
-    if isinstance(model, FeatureViewSourceProtocol):
-        df["is_fv_feature"] = column.name in (
-            c.name for c in model.feature_view_features()
+        _min = cast(func.min(column), String) if is_supported else literal(None)
+        _max = cast(func.max(column), String) if is_supported else literal(None)
+        _unique_count = func.count(column.distinct()) if is_supported else literal(None)
+        _avg = cast(func.avg(column), String) if is_numeric else literal(None)
+        _stddev = func.stddev(column) if is_numeric else literal(None)
+        _null_percentage = (
+            func.safe_divide(
+                (literal(100) * func.countif(column == None)), func.count(column)
+            )
+            if is_supported
+            else literal(None)
         )
-        df["is_fv_entity"] = column.name in (
-            c.name for c in model.feature_view_entities()
+
+        stmt = select(
+            literal(column.name).label("column_name"),
+            literal(str(column.type)).label("column_type"),
+            _min.label("min"),  # type: ignore
+            _max.label("max"),  # type: ignore
+            _unique_count.label("unique_count"),  # type: ignore
+            _avg.label("avg"),  # type: ignore
+            _stddev.label("stddev"),  # type: ignore
+            _null_percentage.label("null_percentage"),  # type: ignore
+            literal(is_fv_feature).label("is_fv_feature"),
+            literal(is_fv_entity).label("is_fv_entity"),
+            literal(is_fv_event_timestamp).label("is_fv_event_timestamp"),
         )
-        df["is_fv_event_timestamp"] = (
-            column.name == model.feature_view_event_timestamp().name
-        )
-    else:
-        feature_view_columns = [
-            "is_fv_feature",
-            "is_fv_entity",
-            "is_fv_event_timestamp",
-        ]
-        df[feature_view_columns] = False
+        stmts.append(stmt)
+
+    all_stmts = union_all(*stmts)
+
+    result = run(all_stmts)
+    df = result.to_dataframe()
 
     return df.replace({nan: None})
