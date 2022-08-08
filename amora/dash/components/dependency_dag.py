82a4f4869d29895dd187819fb5d06e7ebe3e8035
diogommartins <diogommartins.dev@gmail.com>
2022-08-07 22:49:21 -0300

    fix(dash): Fixes `amora.dash.components.dependency_dag` layout

--- amora/dash/components/dependency_dag.py
+++ amora/dash/components/dependency_dag.py
@@ -1,3 +1,4 @@
+import dash_bootstrap_components as dbc
 import dash_cytoscape
 from dash import html
 from dash.development.base_component import Component
@@ -5,23 +6,21 @@ from dash.development.base_component import Component
 from amora.dag import DependencyDAG
 
 
-def component(dag: DependencyDAG) -> Component:
-
-    return html.Div(
+def component(dag: DependencyDAG, height: str = "400px") -> Component:
+    return dbc.Row(
         className="cy-container",
-        # style=styles["cy-container"],
         children=[
             dash_cytoscape.Cytoscape(
                 id="cytoscape-layout",
                 elements=dag.to_cytoscape_elements(),
                 layout={
                     "name": "breadthfirst",
-                    "roots": f'[id = "{dag.root()}"]',
                     "refresh": 20,
                     "fit": True,
                     "padding": 30,
                     "randomize": False,
                 },
+                style={"width": "100%", "height": height},
                 stylesheet=[
                     {"selector": "node", "style": {"label": "data(label)"}},
                     {
