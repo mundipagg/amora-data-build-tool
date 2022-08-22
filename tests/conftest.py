e688cc50b97a67f170feb10803c4f297480c36e2
diogommartins <diogommartins.dev@gmail.com>
2022-08-22 11:47:12 -0300

    tests(ci): Adds `pytest_setup_options` to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from selenium.webdriver.chrome.options import Options
+
 from amora.config import settings
 from amora.utils import clean_compiled_files
 
@@ -13,3 +15,10 @@ def pytest_sessionstart(session):
 
 def pytest_sessionfinish(session, exitstatus):
     clean_compiled_files()
+
+
+def pytest_setup_options():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    return options
