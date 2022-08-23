24d3eece8719c9300705427bb2efbd191619938e
Thúlio Costa <thulio.patriota@stone.com.br>
2022-08-23 17:04:35 -0300

    fix: Corrige, temporariamente, teste test_models

--- tests/dash/pages/test_models.py
+++ tests/dash/pages/test_models.py
@@ -11,9 +11,12 @@ def test_models_page(amora_dash: DashComposite):
         stay_on_page=True,
     )
 
+    amora_dash.find_element("btn-close", "CLASS_NAME").click()
+
     amora_dash.select_dcc_dropdown(
         "div#model-select-dropdown", value=StepCountBySource.unique_name()
     )
+
     amora_dash.wait_for_text_to_equal(
         selector="H4.card-title", text=StepCountBySource.unique_name()
     )
