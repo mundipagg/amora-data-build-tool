5c107204fbaf50785e2bbf4b4e5bc4b5a25db062
Beto <robertom@marinheiro.com.br>
2022-09-26 20:05:49 -0300

    Fix tests of tests/cli/test_materialize

--- tests/cli/test_materialize.py
+++ tests/cli/test_materialize.py
@@ -20,13 +20,19 @@ def teardown_function(module):
     clean_compiled_files()
 
 
+@patch("concurrent.futures.ProcessPoolExecutor")
 @patch("amora.cli.typer_app.compile")
 @patch("amora.materialization.materialize")
 def test_materialize_without_arguments_and_options(
-    materialize: MagicMock, compile: MagicMock
+    materialize: MagicMock, compile: MagicMock, pool_mock: MagicMock
 ):
+    executor_mock = MagicMock()
+    executor_mock.map = MagicMock()
+    pool_mock.return_value.__enter__.return_value = executor_mock
 
-    for model in [HeartRate, Steps]:
+    models = [Steps, HeartRate]
+
+    for model in models:
         target_path = model.target_path(model_file_path=inspect.getfile(model))
         target_path.write_text("SELECT 1")
 
@@ -36,17 +42,24 @@ def test_materialize_without_arguments_and_options(
     )
 
     assert result.exit_code == 0
-
-    tables = sorted(
-        str(call[1]["model"].__table__) for call in materialize.call_args_list
+    executor_mock.map.assert_called_once_with(
+        materialize,
+        ["SELECT 1"] * len(models),
+        [model.unique_name() for model in models],
+        [model.__model_config__ for model in models],
     )
-    assert tables == sorted([str(Steps.__table__), str(HeartRate.__table__)])
     compile.assert_called_once_with(target=None, models=[])
 
 
+@patch("concurrent.futures.ProcessPoolExecutor")
 @patch("amora.cli.typer_app.compile")
 @patch("amora.materialization.materialize")
-def test_materialize_with_model_options(materialize: MagicMock, compile: MagicMock):
+def test_materialize_with_model_options(
+    materialize: MagicMock, compile: MagicMock, pool_mock: MagicMock
+):
+    executor_mock = MagicMock()
+    executor_mock.map = MagicMock()
+    pool_mock.return_value.__enter__.return_value = executor_mock
 
     for model in [HeartRate, Steps]:
         target_path = model.target_path(model_file_path=inspect.getfile(model))
@@ -58,18 +71,20 @@ def test_materialize_with_model_options(materialize: MagicMock, compile: MagicMo
     )
 
     assert result.exit_code == 0
-    materialize_call_model = materialize.mock_calls[0][2]["model"]
-    assert materialize_call_model.__table__ == Steps.__table__
-
+    executor_mock.map.assert_called_once_with(
+        materialize, ["SELECT 1"], [Steps.unique_name()], [Steps.__model_config__]
+    )
     compile.assert_called_once_with(models=["steps"], target=None)
 
 
+@patch("concurrent.futures.ProcessPoolExecutor")
 @patch("amora.cli.typer_app.compile")
 @patch("amora.materialization.materialize")
 @patch("amora.dag.DependencyDAG.draw")
 def test_materialize_with_draw_dag_option(
-    draw: MagicMock, _materialize: MagicMock, _compile: MagicMock
+    draw: MagicMock, _materialize: MagicMock, _compile: MagicMock, _pool_mock: MagicMock
 ):
+
     result = runner.invoke(
         app,
         ["materialize", "--draw-dag"],
@@ -79,9 +94,12 @@ def test_materialize_with_draw_dag_option(
     draw.assert_called_once()
 
 
+@patch("concurrent.futures.ProcessPoolExecutor")
 @patch("amora.cli.typer_app.compile")
 @patch("amora.materialization.materialize")
-def test_materialize_with_no_compile_option(materialize: MagicMock, compile: MagicMock):
+def test_materialize_with_no_compile_option(
+    _materialize: MagicMock, compile: MagicMock, _pool_mock: MagicMock
+):
     result = runner.invoke(
         app,
         ["materialize", "--no-compile"],
