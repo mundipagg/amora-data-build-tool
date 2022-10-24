ddb8327e7930a4aa67fe0d0e7740a0a65b40ffbb
diogommartins <diogommartins.dev@gmail.com>
2022-10-24 09:03:27 -0300

    fix(examples): Fixes step_count_by_source.py group by clause

--- examples/amora_project/models/step_count_by_source.py
+++ examples/amora_project/models/step_count_by_source.py
@@ -38,16 +38,19 @@ class StepCountBySource(AmoraModel):
 
     @classmethod
     def source(cls) -> Optional[Compilable]:
-        datetime_trunc = func.timestamp(datetime_trunc_hour(Steps.creationDate))
+        source_name = Steps.sourceName.label(cls.__table__.columns.source_name.key)
+        event_timestamp = func.timestamp(datetime_trunc_hour(Steps.creationDate)).label(
+            cls.__table__.columns.event_timestamp.key
+        )
         return select(
             [
                 func.avg(Steps.value).label(cls.__table__.columns.value_avg.key),
                 func.sum(Steps.value).label(cls.__table__.columns.value_sum.key),
                 func.count(Steps.value).label(cls.__table__.columns.value_count.key),
-                Steps.sourceName,
-                datetime_trunc.label(cls.__table__.columns.event_timestamp.key),
+                source_name,
+                event_timestamp,
             ]
-        ).group_by(cls.source_name, cls.event_timestamp)
+        ).group_by(source_name, event_timestamp)
 
     @classmethod
     def feature_view_entities(cls):
