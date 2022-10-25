ccc2a6b5fe596ba144539f12f05f6a5513ef6192
diogommartins <diogommartins.dev@gmail.com>
2022-10-25 11:50:23 -0300

    feat(models): Adds `AmoraModel.columns` classmethod

--- amora/models.py
+++ amora/models.py
@@ -22,6 +22,7 @@ from typing import (
 from pydantic import NameEmail
 from sqlalchemy import Column, MetaData, Table
 from sqlalchemy.orm import declared_attr, registry
+from sqlalchemy.sql.base import ImmutableColumnCollection
 
 from amora.config import settings
 from amora.logger import logger
@@ -170,6 +171,14 @@ class AmoraModel:
             cls.__tablename__override__
             or re.sub(r"(?<!^)(?=[A-Z])", "_", cls.__mro__[0].__name__).lower()
         )
+
+    @classmethod
+    def columns(cls) -> ImmutableColumnCollection:
+        if cls.__model_config__.materialized == MaterializationTypes.ephemeral:
+            cte = cls.source()
+            return cte.columns
+        else:
+            return cls.__table__.columns
 
     @classmethod
     def dependencies(cls) -> List[Table]:
