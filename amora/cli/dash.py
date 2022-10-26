df129de09b973717b5fb0deb7b79d73be3deb5d5
Thúlio Costa <thulio.patriota@stone.com.br>
2022-10-26 16:03:16 -0300

    feat: Usa gunicorn em produção

--- amora/cli/dash.py
+++ amora/cli/dash.py
@@ -5,7 +5,38 @@ app = typer.Typer(help="Amora dashboards")
 
 @app.command("serve")
 def serve():
+    import gunicorn.app.base
+
     from amora.dash.app import dash_app
     from amora.dash.config import settings
 
-    dash_app.run(debug=settings.DEBUG, host=settings.HTTP_HOST, port=settings.HTTP_PORT)
+    # https://docs.gunicorn.org/en/latest/custom.html#custom-application
+    class StandaloneApplication(gunicorn.app.base.BaseApplication):
+        def __init__(self, app, options=None):
+            self.options = options or {}
+            self.application = app
+            super().__init__()
+
+        def load_config(self):
+            config = {
+                key: value
+                for key, value in self.options.items()
+                if key in self.cfg.settings and value is not None
+            }
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+
+        def load(self):
+            return self.application
+
+    options = {
+        "bind": f"{settings.HTTP_HOST}:{settings.HTTP_PORT}",
+        "workers": 3,  # FIXME: Configurar via env var?
+        "timeout": 60,  # FIXME: Configurar via env var?
+    }
+    if settings.DEBUG:
+        dash_app.run(
+            debug=settings.DEBUG, host=settings.HTTP_HOST, port=settings.HTTP_PORT
+        )
+    else:
+        StandaloneApplication(dash_app.server, options).run()
