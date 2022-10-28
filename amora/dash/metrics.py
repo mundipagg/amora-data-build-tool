3d25374bc6d2d45997d878ab6fadac614f0b6415
diogommartins <diogommartins.dev@gmail.com>
2022-10-28 14:41:54 -0300

    feat(metrics): Adds metrics related to SPA page load and component update - amora_dash_component_update - amora_dash_pages_location_pathname_change

--- amora/dash/metrics.py
+++ amora/dash/metrics.py
@@ -1,10 +1,84 @@
+from timeit import default_timer
+from typing import Callable
+
 from dash import Dash
+from flask import Response, request
+from prometheus_client import Histogram
 from prometheus_flask_exporter import PrometheusMetrics
 
 from amora.version import VERSION
 
 
+def callback_instrumenter(callback: Callable):
+    def decorator(*args, **kwargs):
+        result = callback(*args, **kwargs)
+        return result
+
+    return decorator
+
+
 def register_metrics(dash: Dash) -> None:
-    metrics = PrometheusMetrics(app=dash.server)
+    flask_app = dash.server
+    metrics = PrometheusMetrics(app=flask_app)
 
     metrics.info("amora_version", "Amora version", version=VERSION)
+
+    pathname_request_duration_metric = Histogram(
+        "amora_dash_pages_location_pathname_change",
+        "Flask HTTP request related to dash's SPA pathname change duration in seconds",
+        ("method", "pathname", "status"),
+        registry=metrics.registry,
+    )
+
+    component_update_request_duration_metric = Histogram(
+        "amora_dash_component_update",
+        "Flask HTTP request related to dash's SPA component update duration in seconds",
+        ("method", "status", "inputs", "output"),
+        registry=metrics.registry,
+    )
+
+    def after_request(response: Response) -> Response:
+        if not hasattr(request, "prom_start_time"):
+            raise ValueError
+        total_time = max(default_timer() - request.prom_start_time, 0)
+
+        if request.path == "/_dash-update-component":
+            payload = request.get_json()
+            if "_pages_location.pathname" in payload["changedPropIds"]:
+                for input in payload["inputs"]:
+                    if (
+                        input["id"] == "_pages_location"
+                        and input["property"] == "pathname"
+                    ):
+                        pathname_request_duration_metric.labels(
+                            method=request.method,
+                            pathname=input["value"],
+                            status=response.status_code,
+                        ).observe(total_time)
+                        return response
+            elif "url.pathname" in payload["changedPropIds"]:
+                for input in payload["inputs"]:
+                    if input["id"] == "url" and input["property"] == "pathname":
+                        pathname_request_duration_metric.labels(
+                            method=request.method,
+                            pathname=input["value"],
+                            status=response.status_code,
+                        ).observe(total_time)
+                        return response
+            else:
+                inputs = ":".join(i["id"] for i in payload["inputs"])
+                component_update_request_duration_metric.labels(
+                    method=request.method,
+                    status=response.status_code,
+                    inputs=inputs,
+                    output=payload["output"],
+                ).observe(total_time)
+                return response
+
+        return response
+
+    def teardown_request(exception=None):
+        pass
+
+    flask_app.after_request(after_request)
+    flask_app.teardown_request(teardown_request)
