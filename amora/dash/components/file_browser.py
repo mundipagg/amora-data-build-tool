e74ab8072ded6a6c0ad63682d68a5178f2837971
diogommartins <diogommartins.dev@gmail.com>
2022-11-21 21:15:41 -0300

    fix(dash): Fixes file browser tree data implementation

--- amora/dash/components/file_browser.py
+++ amora/dash/components/file_browser.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import feffery_antd_components as antd
@@ -7,37 +6,41 @@ from dash.development.base_component import Component
 from amora.config import settings
 
 
-def make_tree(root: Path) -> dict:
-    if root.is_dir():
-        children = root.iterdir()
+def _make_tree(root: Path) -> dict:
+    """
+    Generates a tree from a path.
+    Node schema compatible with ant.design's TreeSelect component
+
+    More on: https://ant.design/components/tree-select
+    """
+    if not root.is_dir() and not root.stem.startswith("_"):
+        return {
+            "title": root.name,
+            "key": root.as_posix(),
+            "icon": "antd-file",
+            "children": [],
+        }
     else:
-        children = []
-    return {"title": Path(root).stem, "key": root.as_posix(), "children": children}
+        return {
+            "title": root.name,
+            "key": root.as_posix(),
+            "icon": "antd-folder",
+            "disabled": True,
+            "children": [
+                _make_tree(file)
+                for file in root.iterdir()
+                if not file.stem.startswith("_")
+            ],
+        }
 
 
 component_id = "file-browser-tree"
 
 
 def layout() -> Component:
-    tree = list(os.walk(settings.models_path))
-    tree_data = [
-        {
-            "title": Path(dirpath).stem,
-            "key": dirpath,
-            "children": [
-                {
-                    "title": file,
-                    "key": Path(dirpath).joinpath(file).as_posix(),
-                    "icon": "antd-table",
-                }
-                for file in filenames
-            ],
-        }
-        for (dirpath, dirnames, filenames) in tree
-    ]
-
     return antd.AntdTree(
-        treeData=tree_data,
         id=component_id,
         defaultExpandAll=True,
+        showIcon=True,
+        treeData=[_make_tree(settings.models_path)],
     )
