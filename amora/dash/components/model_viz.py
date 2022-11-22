988e15be9aeb3e868038de6934f83a60a2f40c7a
diogommartins <diogommartins.dev@gmail.com>
2022-11-22 16:00:15 -0300

    fix(dash): Fixes summary visualization bins

--- amora/dash/components/model_viz.py
+++ amora/dash/components/model_viz.py
@@ -1,22 +1,19 @@
-from typing import Callable, List, Any, Optional
+from typing import Any, Callable, List, Optional
 
-import pandas as pd
-
-from numerize.numerize import numerize
-
-from pandas.api.types import (
-    is_string_dtype,
-    is_numeric_dtype,
-    is_datetime64_any_dtype,
-    is_bool_dtype,
-)
-
-import plotly.express as px
-from plotly.graph_objects import Figure
-
-from dash import html, dcc
-from dash.development.base_component import Component
 import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly
+import plotly.express as px
+from dash import dcc, html
+from dash.development.base_component import Component
+from pandas.api.types import (
+    is_array_like,
+    is_bool_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_string_dtype,
+)
+from plotly.graph_objects import Figure
 
 from amora.models import Model
 from amora.providers.bigquery import sample
@@ -39,24 +36,19 @@ def figure_apply_default_layout(figure: Figure) -> Figure:
 
 
 def value_counts_series_to_dataframe(series: pd.Series) -> pd.DataFrame:
-    df_value_counts = series.to_frame().reset_index()
-    df_value_counts.set_axis(["x", "count"], axis=1, inplace=True)
-    return df_value_counts
+    return series.to_frame().reset_index().set_axis(["x", "count"], axis=1)
 
 
 def cut_formatter(cut: pd.Series, formatter_function: Callable) -> list[str]:
     """
     Apply a formatter function in both `left` and `right` Interval.
     Sometimes the pd.cuts generates long Interval strings, and we may
-    want to format each part of it. 
+    want to format each part of it.
     """
-    humanized_cut = []
-    for _, interval in cut.iteritems():
-        humanized_cut.append(
-            f"{formatter_function(interval.left)}-{formatter_function(interval.right)}"
-        )
-
-    return humanized_cut
+    return [
+        f"{formatter_function(interval.left)}-{formatter_function(interval.right)}"
+        for _, interval in cut.iteritems()
+    ]
 
 
 def get_binned_dataframe(
@@ -64,16 +56,19 @@ def get_binned_dataframe(
     formatter_function: Optional[Callable] = None,
 ) -> pd.DataFrame:
     bins = min(series.nunique(), 10)
-    cut = pd.cut(x=series, bins=bins, include_lowest=True, precision=0)
+    cut = pd.cut(x=series, bins=bins, include_lowest=True, precision=0, right=False)
 
     if formatter_function:
         cut = cut_formatter(cut, formatter_function)
 
     frequencies = pd.value_counts(cut, sort=False)
 
-    df_frequencies = frequencies.to_frame().reset_index()
-
-    df_frequencies.set_axis(["x", "count"], axis=1, inplace=True)
+    df_frequencies = (
+        frequencies.to_frame()
+        .reset_index()
+        .set_axis(["x", "count"], axis=1)
+        .sort_values(by="x")
+    )
 
     df_frequencies["x"] = df_frequencies["x"].astype(str)
 
@@ -94,18 +89,33 @@ def datetime_series_aggregation(series: pd.Series) -> pd.Series:
 
 
 def create_component_for_one_unique_value(series: pd.Series) -> html.P:
-
     return html.P(f"One unique value: {series[0]}", style={"color": "red"})
 
 
 def get_most_common_values(
     df_value_counts_normalized: pd.DataFrame,
-) -> List[html.P]:
+) -> List[Component]:
 
     elements = []
 
     for _, row in df_value_counts_normalized.head(2).iterrows():
-        elements.append(html.P(f"{row['x']}        {round((row['count']*100), 2)}%"))
+        percentage = round((row["count"] * 100), 2)
+        elements.append(
+            dbc.Row(
+                [
+                    dbc.Col(
+                        f"{row['x']}",
+                        style={
+                            "text-overflow": "ellipsis",
+                            "overflow": "hidden",
+                            "white-space": "nowrap",
+                            "max-width": "200px",
+                        },
+                    ),
+                    dbc.Col(html.B(f"{percentage}%")),
+                ]
+            )
+        )
 
     if df_value_counts_normalized.shape[0] > 2:
         other = round(
@@ -119,7 +129,7 @@ def get_most_common_values(
             * 100,
             2,
         )
-        elements.append(html.P(f"other        {other}%"))
+        elements.append(dbc.Row([dbc.Col("other"), dbc.Col(html.B(f"{other}%"))]))
 
     return elements
 
@@ -143,20 +153,19 @@ def create_bar_plot(
 
 def create_component_bool_type(series: pd.Series) -> dcc.Graph:
     series_value_counts = series.value_counts()
+    series_value_counts.index = series_value_counts.index.map(str)
     df_value_counts = value_counts_series_to_dataframe(series_value_counts)
     return create_bar_plot(
         df_value_counts,
-        color="x",
-        barmode="stack",
     )
 
 
 def create_component_numeric_type(series: pd.Series) -> dcc.Graph:
-    df_bin = get_binned_dataframe(series, numerize)
-    return create_bar_plot(
-        df_bin,
-    )
-
+    if series.nunique() <= 2:
+        return create_component_bool_type(series)
+    else:
+        figure = plotly.hist_series(series)
+        return dcc.Graph(figure=figure_apply_default_layout(figure))
 
 def create_component_datetime_type(series: pd.Series) -> dcc.Graph:
     datetime_series_agg = datetime_series_aggregation(series)
@@ -175,49 +184,48 @@ def create_component_string_type(series: pd.Series) -> dcc.Graph:
     return get_most_common_values(df_value_counts_normalized)
 
 
-def create_df_visualizations(df: pd.DataFrame) -> Component:
-    col_list: list = []
-
-    for column_name in df:
-        column_name = str(column_name)
-        column_series: pd.Series = df[column_name]
-
-        if column_series.nunique() == 1:
-            component_title = column_name
-            component_viz = create_component_for_one_unique_value(column_series)
-        else:
-            if is_bool_dtype(column_series):
-                component_title = f"✔️ {column_name}"
-                component_viz = create_component_bool_type(column_series)
-
-            elif is_numeric_dtype(column_series):
-                component_title = f"🔢 {column_name}"
-                component_viz = create_component_numeric_type(column_series)
-
-            elif is_datetime64_any_dtype(column_series):
-                component_title = f"📅 {column_name}"
-                component_viz = create_component_datetime_type(column_series)
-
-            elif is_string_dtype(column_series):
-                component_title = f"🔤 {column_name}"
-                component_viz = create_component_string_type(column_series)
-
-            else:
-                component_title = f"❓ {column_name}"
-                component_viz = html.P("No viz implemented for this type of column 😔")
-
-        col_list.append(
-            dbc.Col(
-                [
-                    html.Div(
-                        [
-                            html.B(component_title),
-                            html.Div(component_viz),
-                        ]
-                    )
-                ]
-            )
+def visualization_for_series(series: pd.Series) -> dbc.Card:
+    if is_array_like(series[0]):
+        component_title = f"⚠️ {series.name}"
+        component_viz = dbc.Alert(
+            "Visualization not implemented for arrays", color="info"
         )
+    elif series.nunique() == 1:
+        component_title = series.name
+        component_viz = create_component_for_one_unique_value(series)
+    elif is_bool_dtype(series):
+        component_title = f"✔️ {series.name}"
+        component_viz = create_component_bool_type(series)
+
+    elif is_numeric_dtype(series):
+        component_title = f"🔢 {series.name}"
+        component_viz = create_component_numeric_type(series)
+
+    elif is_datetime64_any_dtype(series):
+        component_title = f"📅 {series.name}"
+        component_viz = create_component_datetime_type(series)
+
+    elif is_string_dtype(series):
+        component_title = f"🔤 {series.name}"
+        component_viz = create_component_string_type(series)
+    else:
+        component_title = f"❓ {series.name}"
+        component_viz = html.P("No viz implemented for this type of column 😔")
+
+    return dbc.Card(
+        [dbc.CardHeader(component_title), dbc.CardBody(component_viz)],
+        className="w-75 mb-3",
+    )
+
+
+def create_df_visualizations(df: pd.DataFrame) -> Component:
+    col_list = [
+        dbc.Col(
+            visualization_for_series(series=df[column_name]),
+            width=4,
+        )
+        for column_name in df
+    ]
 
     return dbc.Row(col_list)
 
@@ -230,4 +238,14 @@ def component(model: Model) -> Component:
             f"This component depends Sample and it is not implemented for model {model.unique_name}"
         )
     else:
-        return create_df_visualizations(df)
+        return dbc.Row(
+            [
+                dbc.Row(
+                    dbc.Alert(
+                        "The visualizations bellow are generated from the model sample data",
+                        color="info",
+                    )
+                ),
+                dbc.Row(create_df_visualizations(df)),
+            ]
+        )
