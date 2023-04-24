9210ddecccb6beffe9a6e9a1b7444c981a3873db
Diogo Magalhães Machado <diogommartins.dev@gmail.com>
2023-04-24 08:32:28 -0300

    feat(dashboards): Adds `amora.dashboard.Dashboard.filter`

--- amora/dashboards.py
+++ amora/dashboards.py
@@ -1,8 +1,8 @@
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Any, Optional, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from amora.config import settings
 from amora.filters import Filter
@@ -73,6 +73,64 @@ class Dashboard(BaseModel):
 
     class Config:
         arbitrary_types_allowed = True
+
+    def to_markdown(self):
+        """
+        Converts the dashboard and all its questions to a Markdown-formatted string.
+        """
+        def gen_sections():
+            yield f"# {self.name}"
+            yield "---"
+            for row in self.questions:
+                for question in row:
+                    yield "---"
+                    yield question.to_markdown()
+
+        return "\n".join(gen_sections())
+
+    def _apply_filters(self, question: Question, **kwargs: Dict[str, Any]) -> Question:
+        for field, value in kwargs.items():
+            filter = self._get_filter(field)
+            if filter.is_valid_for(question):
+                question = filter.filter(question, value=value)
+        return question
+
+    def _get_filter(self, field: str) -> Filter:
+        for filter in self.filters:
+            if filter.field == field:
+                return filter
+        raise ValueError(f"Invalid field `{field}`")
+
+    def filter(self, **kwargs: Dict[str, Any]) -> "Dashboard":
+        """
+        Returns a new Dashboard object with filters applied.
+
+        Args:
+            **kwargs: keyword arguments representing filters to apply on the dashboard
+
+        Raises:
+            ValueError: If a filter is not valid for a question
+
+        Returns:
+            A new Dashboard object with filters applied
+
+        """
+        questions = []
+        for row in self.questions:
+            filtered_row = []
+            for question in row:
+                filtered_question = self._apply_filters(question, **kwargs)
+                filtered_row.append(filtered_question)
+            questions.append(filtered_row)
+
+        new_uid = f"{self.uid}_{'__'.join(f'{field}_{value}' for field, value in kwargs.items())}"
+
+        return Dashboard(
+            questions=questions,
+            filters=self.filters,
+            name=self.name,
+            uid=new_uid
+        )
 
 
 DASHBOARDS: Dict[DashboardUid, Dashboard] = {}
